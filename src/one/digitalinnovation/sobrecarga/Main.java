1c3672fbb724bd89a23b717b624c7e6b9b365b79
alcid <alcides_thiago@hotmail.com>
2022-08-07 16:14:02 -0300

    Includ metod Quadrilatero.java

---
+++ src/one/digitalinnovation/sobrecarga/Main.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.sobrecarga;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Calculos Quadrilateros");
+        Quadrilatero.area(2);
+        Quadrilatero.area(5,5);
+        Quadrilatero.area(7,8,9);
+
+    }
+}
